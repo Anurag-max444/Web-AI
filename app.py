89a5547b767f44ca2928bc9970060663f1f96d89
Anurag-max444 <anuragvishwakarma6233@gmail.com>
2026-03-26 14:46:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, session
 import json
 
 app = Flask(__name__)
-app.secret_key = "secret123"  # change later
+app.secret_key = "SECRET_KEY"
 
 # load tools
 def load_tools():
